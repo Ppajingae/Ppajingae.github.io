bcde8b7b0cdbe5efb47682142489a4fcbfbe479e
Ppajingae <wkmkyj991112@gmail.com>
2024-10-13 17:28:17 +0900

    delete next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // output: 'export',
-    // basePath: '/ppajingae.github.io',
-}
+const nextConfig = {}
 
 export default nextConfig;
