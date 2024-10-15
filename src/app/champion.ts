f786aef9227b54c7183bb9e4fea613569f5dc086
Ppajingae <wkmkyj991112@gmail.com>
2024-10-15 16:14:21 +0900

    update champion

--- src/app/champion.ts
+++ src/app/champion.ts
@@ -101,5 +101,140 @@ export default function championData(): Champion[] {
             name: "드레이븐",
             image: "draven.png"
         },
+        {
+            number: 20,
+            name: "라이즈",
+            image: "ryze.png"
+        },
+        {
+            number: 21,
+            name: "라칸",
+            image: "rakan.png"
+        },
+        {
+            number: 22,
+            name: "람머스",
+            image: "rammus.png"
+        },
+        {
+            number: 23,
+            name: "럭스",
+            image: "lux.png"
+        },
+        {
+            number: 24,
+            name: "럼블",
+            image: "rumble.png"
+        },
+        {
+            number: 25,
+            name: "레나타 글라스크",
+            image: "renata_glasc.png"
+        },
+        {
+            number: 26,
+            name: "레넥톤",
+            image: "renekton.png"
+        },
+        {
+            number: 27,
+            name: "레오나",
+            image: "leona.png"
+        },
+        {
+            number: 28,
+            name: "렉사이",
+            image: "reksai.png"
+        },
+        {
+            number: 29,
+            name: "렐",
+            image: "rell.png"
+        },
+        {
+            number: 30,
+            name: "렝가",
+            image: "rengar.png"
+        },
+        {
+            number: 31,
+            name: "루시안",
+            image: "lucian.png"
+        },
+        {
+            number: 32,
+            name: "룰루",
+            image: "lulu.png"
+        },
+        {
+            number: 33,
+            name: "르블랑",
+            image: "leblanc.png"
+        },
+        {
+            number: 34,
+            name: "리신",
+            image: "leesin.png"
+        },
+        {
+            number: 35,
+            name: "리븐",
+            image: "riven.png"
+        },
+        {
+            number: 36,
+            name: "리산드라",
+            image: "lissandra.png"
+        },
+        {
+            number: 37,
+            name: "릴리아",
+            image: "lillia.png"
+        },
+        {
+            number: 38,
+            name: "마스터 이",
+            image: "masteryi.png"
+        },
+        {
+            number: 39,
+            name: "마오카이",
+            image: "maokai.png"
+        },
+        {
+            number: 40,
+            name: "말자하",
+            image: "malzahar.png"
+        },
+        {
+            number: 41,
+            name: "말파이트",
+            image: "malphite.png"
+        },
+        {
+            number: 42,
+            name: "모데카이저",
+            image: "mordekaiser.png"
+        },
+        {
+            number: 43,
+            name: "모르가나",
+            image: "morgana.png"
+        },
+        {
+            number: 44,
+            name: "문도 박사",
+            image: "dr.mundo.png"
+        },
+        {
+            number: 45,
+            name: "미스 포츈",
+            image: "miss_fortune.png"
+        },
+        {
+            number: 46,
+            name: "밀리오",
+            image: "milio.png"
+        },
     ]
 }
