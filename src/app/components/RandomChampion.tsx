e7acfc1714b9f980685deec3f1715ac53bff01fd
Ppajingae <wkmkyj991112@gmail.com>
2024-10-15 17:50:06 +0900

    code refactoring

---
+++ src/app/components/RandomChampion.tsx
@@ -0,0 +1,44 @@
+'use client'
+import {useState} from "react";
+import championData from "@/app/champion";
+
+interface Champion {
+    number: number;
+    name: string;
+    image: string;
+}
+
+export default function RandomChampion(){
+
+    const [randomArg, setRandomArg] = useState({
+        number: 0,
+        name: "랜덤으로 돌려 주세요",
+        image: "random.png"
+    });
+
+    const champion : Champion[] = championData();
+
+    function getRandomChampion(): Champion {
+        const randomNumber =  Math.floor(Math.random() * champion.length - 1) + 1;
+
+        return champion[randomNumber];
+    }
+
+
+    return(
+        <div>
+            <img
+                src={`/image/${randomArg.image}`}
+                alt={randomArg.name}
+                width={200}
+                height={200}/>
+            <p>{randomArg.name}</p>
+            <button
+                onClick={() => {
+                    setRandomArg(getRandomChampion())
+                }}
+            >변경
+            </button>
+        </div>
+    )
+}
--- src/app/solo/page.tsx
+++ src/app/solo/page.tsx
@@ -1,45 +1,11 @@
-'use client'
-import { useState } from "react";
 import "./solo.css"
-import championData from "@/app/champion";
-
-
-interface Champion {
-    number: number;
-    name: string;
-    image: string;
-}
+import RandomChampion from "@/app/components/RandomChampion";
 
 export default function Home() {
 
-    const [randomArg, setRandomArg] = useState({
-        number: 0,
-        name: "랜덤으로 돌려 주세요",
-        image: "random.png"
-    });
-
-    const champion : Champion[] = championData();
-
-    function getRandomChampion(): Champion {
-        const randomNumber =  Math.floor(Math.random() * champion.length - 1) + 1;
-
-        return champion[randomNumber];
-    }
-
   return (
       <div className="global">
-          <img
-              src={`/image/${randomArg.image}`}
-              alt={randomArg.name}
-              width={200}
-              height={200}/>
-          <p>{randomArg.name}</p>
-          <button
-              onClick={() => {
-                  setRandomArg(getRandomChampion())
-              }}
-          >변경
-          </button>
+          <RandomChampion/>
       </div>
   );
 }
