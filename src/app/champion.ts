180f724f9097e29c1830ee72efd5d83a8a8f6b14
Ppajingae <wkmkyj991112@gmail.com>
2024-10-29 22:46:57 +0900

    롤 챔피언 ㅅ 까지 추가

--- src/app/champion.ts
+++ src/app/champion.ts
@@ -1,5 +1,6 @@
 interface Champion {
     number: number;
+    championNumber: number;
     name: string;
     image: string;
 }
@@ -8,318 +9,503 @@ export default function championData(): Champion[] {
 
     return [
         {
-            number: 50,
+            number: 1,
+            championNumber: 50,
             name: "가렌",
             image: "garan.png"
         },
         {
-            number: 57,
+            number: 2,
+            championNumber: 57,
             name: "갈리오",
             image: "galio.png"
         },
         {
-            number: 30,
+            number: 3,
+            championNumber: 30,
             name: "갱플랭크",
             image: "gangplank.png"
         },
         {
-            number: 44,
+            number: 4,
+            championNumber: 44,
             name: "그라가스",
             image: "gragas.png"
         },
         {
-            number: 85,
+            number: 5,
+            championNumber: 85,
             name: "그레이브즈",
             image: "graves.png"
         },
         {
-            number: 155,
+            number: 6,
+            championNumber: 155,
             name: "그웬",
             image: "gwen.png"
         },
         {
-            number: 120,
+            number: 7,
+            championNumber: 120,
             name: "나르",
             image: "gnar.png"
         },
         {
-            number: 108,
+            number: 8,
+            championNumber: 108,
             name: "나미",
             image: "nami.png"
         },
         {
-            number: 38,
+            number: 9,
+            championNumber: 38,
             name: "나서스",
             image: "nasus.png"
-        },    {
-            number: 164,
+        },
+        {
+            number: 10,
+            championNumber: 164,
             name: "나피리",
             image: "naafiri.png"
         },
         {
-            number: 93,
+            number: 11,
+            championNumber: 93,
             name: "노틸러스",
             image: "nautilus.png"
         },
         {
-            number: 72,
+            number: 12,
+            championNumber: 72,
             name: "녹턴",
             image: "nocturne.png"
         },
         {
-            number: 1,
+            number: 13,
+            championNumber: 1,
             name: "누누",
             image: "nunu.png"
         },
         {
-            number: 42,
+            number: 14,
+            championNumber: 42,
             name: "니달리",
             image: "nidalee.png"
         },
         {
-            number: 142,
+            number: 15,
+            championNumber: 142,
             name: "니코",
             image: "neeko.png"
         },
         {
-            number: 161,
+            number: 16,
+            championNumber: 161,
             name: "닐라",
             image: "nilah.png"
         },
         {
-            number: 98,
+            number: 17,
+            championNumber: 98,
             name: "다리우스",
             image: "darius.png"
         },
         {
-            number: 102,
+            number: 18,
+            championNumber: 102,
             name: "다이애나",
             image: "diana.png"
         },
         {
-            number: 99,
+            number: 19,
+            championNumber: 99,
             name: "드레이븐",
             image: "draven.png"
         },
         {
-            number: 2,
+            number: 20,
+            championNumber: 2,
             name: "라이즈",
             image: "ryze.png"
         },
         {
-            number: 136,
+            number: 21,
+            championNumber: 136,
             name: "라칸",
             image: "rakan.png"
         },
         {
-            number: 26,
+            number: 22,
+            championNumber: 26,
             name: "람머스",
             image: "rammus.png"
         },
         {
-            number: 62,
+            number: 23,
+            championNumber: 62,
             name: "럭스",
             image: "lux.png"
         },
         {
-            number: 75,
+            number: 24,
+            championNumber: 75,
             name: "럼블",
             image: "rumble.png"
         },
         {
-            number: 159,
+            number: 25,
+            championNumber: 159,
             name: "레나타 글라스크",
             image: "renata_glasc.png"
         },
         {
-            number: 68,
+            number: 26,
+            championNumber: 68,
             name: "레넥톤",
             image: "renekton.png"
         },
         {
-            number: 79,
+            number: 27,
+            championNumber: 79,
             name: "레오나",
             image: "leona.png"
         },
         {
-            number: 123,
+            number: 28,
+            championNumber: 123,
             name: "렉사이",
             image: "reksai.png"
         },
         {
-            number: 153,
+            number: 29,
+            championNumber: 153,
             name: "렐",
             image: "rell.png"
         },
         {
-            number: 103,
+            number: 30,
+            championNumber: 103,
             name: "렝가",
             image: "rengar.png"
         },
         {
-            number: 115,
+            number: 31,
+            championNumber: 115,
             name: "루시안",
             image: "lucian.png"
         },
         {
-            number: 95,
+            number: 32,
+            championNumber: 95,
             name: "룰루",
             image: "lulu.png"
         },
         {
-            number: 63,
+            number: 33,
+            championNumber: 63,
             name: "르블랑",
             image: "leblanc.png"
         },
         {
-            number: 73,
+            number: 34,
+            championNumber: 73,
             name: "리 신",
             image: "leesin.png"
         },
         {
-            number: 83,
+            number: 35,
+            championNumber: 83,
             name: "리븐",
             image: "riven.png"
         },
         {
-            number: 113,
+            number: 36,
+            championNumber: 113,
             name: "리산드라",
             image: "lissandra.png"
         },
         {
-            number: 149,
+            number: 37,
+            championNumber: 149,
             name: "릴리아",
             image: "lillia.png"
         },
         {
-            number: 3,
+            number: 38,
+            championNumber: 3,
             name: "마스터 이",
             image: "masteryi.png"
         },
         {
-            number: 70,
+            number: 39,
+            championNumber: 70,
             name: "마오카이",
             image: "maokai.png"
         },
         {
-            number: 52,
+            number: 40,
+            championNumber: 52,
             name: "말자하",
             image: "malzahar.png"
         },
         {
-            number: 32,
+            number: 41,
+            championNumber: 32,
             name: "말파이트",
             image: "malphite.png"
         },
         {
-            number: 46,
+            number: 42,
+            championNumber: 46,
             name: "모데카이저",
             image: "mordekaiser.png"
         },
         {
-            number: 4,
+            number: 43,
+            championNumber: 4,
             name: "모르가나",
             image: "morgana.png"
         },
         {
-            number: 33,
+            number: 44,
+            championNumber: 33,
             name: "문도 박사",
             image: "dr.mundo.png"
         },
         {
-            number: 59,
+            number: 45,
+            championNumber: 59,
             name: "미스 포츈",
             image: "miss_fortune.png"
         },
         {
-            number: 163,
+            number: 46,
+            championNumber: 163,
             name: "밀리오",
             image: "milio.png"
         },
         {
-            number: 124,
+            number: 47,
+            championNumber: 124,
             name: "바드",
             image: "bard.png"
         },
         {
-            number: 97,
+            number: 48,
+            championNumber: 97,
             name: "바루스",
             image: "varus.png"
         },
         {
-            number: 109,
+            number: 49,
+            championNumber: 109,
             name: "바이",
             image: "vi.png"
         },
         {
-            number: 28,
+            number: 50,
+            championNumber: 28,
             name: "베이가",
             image: "veigar.png"
         },
         {
-            number: 76,
+            number: 51,
+            championNumber: 76,
             name: "베인",
             image: "vayne.png"
         },
         {
-            number: 157,
+            number: 52,
+            championNumber: 157,
             name: "벡스",
             image: "vex.png"
         },
         {
-            number: 160,
+            number: 53,
+            championNumber: 160,
             name: "벨베스",
             image: "belveth.png"
         },
         {
-            number: 118,
+            number: 54,
+            championNumber: 118,
             name: "벨코즈",
             image: "velkoz.png"
         },
         {
-            number: 88,
+            number: 55,
+            championNumber: 88,
             name: "볼리베어",
             image: "volibear.png"
         },
         {
-            number: 119,
+            number: 56,
+            championNumber: 119,
             name: "브라움",
             image: "braum.png"
         },
         {
-            number: 165,
+            number: 57,
+            championNumber: 165,
             name: "브라이어",
             image: "briar.png"
         },
         {
-            number: 74,
+            number: 58,
+            championNumber: 74,
             name: "브랜드",
             image: "brand.png"
         },
         {
-            number: 56,
+            number: 59,
+            championNumber: 56,
             name: "블라디미르",
             image: "vladimir.png"
         },
         {
-            number: 34,
+            number: 60,
+            championNumber: 34,
             name: "블리츠크랭크",
             image: "blitzcrank.png"
         },
         {
-            number: 154,
+            number: 61,
+            championNumber: 154,
             name: "비에고",
             image: "viego.png"
         },
         {
-            number: 90,
+            number: 62,
+            championNumber: 90,
             name: "빅토르",
             image: "viktor.png"
         },
         {
-            number: 43,
+            number: 63,
+            championNumber: 43,
             name: "뽀삐",
             image: "poppy.png"
         },
+        {
+            number: 64,
+            championNumber: 151,
+            name: "사미라",
+            image: "samira.png"
+        },
+        {
+            number: 65,
+            championNumber: 5,
+            name: "사이온",
+            image: "sion.png"
+        },
+        {
+            number: 66,
+            championNumber: 143,
+            name: "사일러스",
+            image: "sylas.png"
+        },
+        {
+            number: 67,
+            championNumber: 39,
+            name: "샤코",
+            image: "shaco.png"
+        },
+        {
+            number: 68,
+            championNumber: 146,
+            name: "세나",
+            image: "senna.png"
+        },
+        {
+            number: 69,
+            championNumber: 152,
+            name: "세라핀",
+            image: "serapine.png"
+        },
+        {
+            number: 70,
+            championNumber: 91,
+            name: "세주아니",
+            image: "sejuani.png"
+        },
+        {
+            number: 71,
+            championNumber: 148,
+            name: "세트",
+            image: "sett.png"
+        },
+        {
+            number: 72,
+            championNumber: 60,
+            name: "소나",
+            image: "sona.png"
+        },
+        {
+            number: 73,
+            championNumber: 6,
+            name: "소라카",
+            image: "soraka.png"
+        },
+        {
+            number: 74,
+            championNumber: 48,
+            name: "쉔",
+            image: "shen.png"
+        },
+        {
+            number: 75,
+            championNumber: 86,
+            name: "쉬바나",
+            image: "shyvana.png"
+        },
+        {
+            number: 76,
+            championNumber: 167,
+            name: "스몰더",
+            image: "smolder.png"
+        },
+        {
+            number: 77,
+            championNumber: 61,
+            name: "스웨인",
+            image: "swain.png"
+        },
+        {
+            number: 78,
+            championNumber: 81,
+            name: "스카너",
+            image: "skarner.png"
+        },
+        {
+            number: 79,
+            championNumber: 7,
+            name: "시비르",
+            image: "sivir.png"
+        },
+        {
+            number: 80,
+            championNumber: 55,
+            name: "신 짜오",
+            image: "xinzhao.png"
+        },
+        {
+            number: 81,
+            championNumber: 104,
+            name: "신드라",
+            image: "syndra.png"
+        },
+        {
+            number: 82,
+            championNumber: 18,
+            name: "신지드",
+            image: "singed.png"
+        },
+        {
+            number: 83,
+            championNumber: 110,
+            name: "쓰레쉬",
+            image: "thresh.png"
+        },
+
     ]
 }
